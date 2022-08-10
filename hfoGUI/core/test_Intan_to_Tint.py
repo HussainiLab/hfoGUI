bfd8d0cfb42e18ba50f23ae6807cae765503e958
Oliver Shetler <oliver@olivershetler.com>
2022-08-10 17:05:51 -0400

    Added a dummy test for intan_to_egf function and added fully functional test for down_sample_timeseries.

--- hfoGUI/core/test_Intan_to_Tint.py
+++ hfoGUI/core/test_Intan_to_Tint.py
@@ -1,6 +1,12 @@
 from .load_intan_rhd_format.load_intan_rhd_format import read_rhd_data
 
+from .Intan_to_Tint import (
+    intan_to_egf
+    ,down_sample_timeseries
+)
+
 import os
+import numpy as np
 import pytest
 
 
@@ -20,6 +26,39 @@ def test_read_rhd_data():
 
     data = read_rhd_data(base_dir + '/hfoGUI/core/load_intan_rhd_format/sampledata.rhd')
 
-    assert dict == type(data)
+    for header in data.keys():
+        print(header)
+        print(data[header])
+
+    #Check the types of the data we will use.
+    assert type(data) == dict
+    assert type(data['t_amplifier']) == np.ndarray
+    assert type(data['amplifier_data']) == np.ndarray
+    assert type(data['spike_triggers']) == list
+    for channel in data['spike_triggers']:
+        assert type(channel) == dict
 
 
+def test_intan_to_egf():
+    """
+    Test that the intan data is converted to egf.
+    """
+    base_dir = os.getcwd().replace('\\','/')
+    intan_data = read_rhd_data(base_dir + '/hfoGUI/core/load_intan_rhd_format/sampledata.rhd')
+    egf_data = intan_to_egf(intan_data)
+
+    assert egf_data == None
+
+
+def test_down_sample_timeseries():
+
+    data = np.arange(0,np.random.randint(0,5000))
+
+    sample_rate = np.random.uniform(1,5000)
+
+    new_sample_rate = np.random.uniform(1,sample_rate)
+
+    downsampled_data = down_sample_timeseries(data, sample_rate, new_sample_rate)
+
+    # assert that the length of downsampled_data is within + or - 1 of the formula.
+    assert len(downsampled_data) <= len(data) / (sample_rate / new_sample_rate) + 1 and len(downsampled_data) >= len(data) / (sample_rate / new_sample_rate) - 1
