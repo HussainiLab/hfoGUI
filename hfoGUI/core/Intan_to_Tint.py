432874bd0fdf8838a797045d482c90405b314ae9
Oliver Shetler <oliver@olivershetler.com>
2022-08-11 16:16:48 -0400

    Added functions intan_ephys_to_egf_dict and intan_to_egf_header_dict. Renamed intan_to_egf_dict as intan_to_egf_dicts and refactored it so that most of its contents are now in intan_ephys_to_egf_dict. Then created a dummy function to go by the name intan_to_egf_header_dict.

--- hfoGUI/core/Intan_to_Tint.py
+++ hfoGUI/core/Intan_to_Tint.py
@@ -2,17 +2,47 @@ from .load_intan_rhd_format.load_intan_rhd_format import read_rhd_data
 
 import numpy as np
 
-def intan_to_egf(intan_data: dict):
+
+
+
+def intan_to_egf_dicts(intan_data: dict) -> dict:
     """
-    Convert intan data to set.
+    Collects all the data needed for an egf file
+    from the intan data and returns a dictionary
+    ready for writing to an egf file for every channel.
+
+    IMPORTANT NOTE: This function only collects
+    electrophysiology data from the
+    'amplifier_data' key. It does not collect
+    signals from auxiliary inputs.
     """
-    amplifier_data = intan_data['amplifier_data']
+
+    egf_ephys_data = intan_ephys_to_egf_dict(intan_data)
+
+    egf_header = intan_to_egf_header_dict(intan_data)
+
+    return egf_ephys_data, egf_header
+
+
+def intan_ephys_to_egf_dict(intan_data: dict) -> dict:
 
     amplifier_sample_rate = float(intan_data['frequency_parameters']['amplifier_sample_rate'])
 
     egf_sample_rate = 4.8e3
 
-    return None
+    try:
+        assert len(intan_data['amplifier_data']) == len(intan_data['amplifier_channels'])
+    except AssertionError:
+        raise ValueError('The number of amplifier channels does not match the number of custom channel names. This file may be corrupted.')
+
+    egf_ephys_data = dict()
+    egf_ephys_data['time'] = down_sample_timeseries(intan_data['t_amplifier'].flatten(), amplifier_sample_rate, egf_sample_rate)
+    for i in range(len(intan_data['amplifier_data'])):
+        if intan_data['amplifier_data'][i].size > 1:
+            egf_ephys_data[intan_data["amplifier_channels"][i]["custom_channel_name"]] = down_sample_timeseries(intan_data['amplifier_data'][i], amplifier_sample_rate, egf_sample_rate)
+
+    return egf_ephys_data
+
 
 
 def down_sample_timeseries(data: np.ndarray, sample_rate: float, new_sample_rate: float):
@@ -47,7 +77,7 @@ def down_sample_timeseries(data: np.ndarray, sample_rate: float, new_sample_rate
     data = data.flatten()
 
     #Generate a list of floating points where the new
-    # data would ideally be sample from.
+    # data would ideally be sampled from.
     subsample = np.arange(0, data.size, skip_size)
     #Find the nearest index value to the subsample.
     new_index = [int(round(i)) for i in subsample]
@@ -59,3 +89,12 @@ def down_sample_timeseries(data: np.ndarray, sample_rate: float, new_sample_rate
     downsampled_data = data[new_index]
 
     return downsampled_data
+
+
+
+def intan_to_egf_header_dict(intan_data: dict) -> dict:
+    """
+    """
+    egf_header = dict()
+
+    return egf_header
