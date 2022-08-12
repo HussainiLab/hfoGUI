138f67d59233bb9567d9eb51035aba9ef188e784
Oliver Shetler <oliver@olivershetler.com>
2022-08-12 11:14:32 -0400

    Changed all the references to egf in function names and variable names to lfp; added if statements allowing each function to either do egf or eeg depending on settnings. Added dummy values to lfp_hdader. These may not work; this version of Intan_to_Tint should be regarded as broken.

--- hfoGUI/core/Intan_to_Tint.py
+++ hfoGUI/core/Intan_to_Tint.py
@@ -3,9 +3,9 @@ from .load_intan_rhd_format.load_intan_rhd_format import read_rhd_data
 import numpy as np
 
 
-def write_axona_lfp(filepath, egf_ephys_data, egf_header):
+def write_axona_lfp(filepath, lfp_ephys_data, lfp_header):
     session_path, session_filename = os.path.split(filepath)
-    num_samples = int(len(data))
+    num_samples = int(len(lfp_ephys_data))
     trial_date, trial_time = session_datetime(session_filename)
     if '.egf' in session_filename:
         egf = True
@@ -15,21 +15,21 @@ def write_axona_lfp(filepath, egf_ephys_data, egf_header):
     with open(filepath, 'w') as f:
         date = 'trial_date %s' % (trial_date)
         time_head = '\ntrial_time %s' % (trial_time)
-        experimenter = '\nexperimenter %s' % (session_parameters['experimenter'])
-        comments = '\ncomments %s' % (session_parameters['comments'])
-        duration = '\nduration %d' % (session_parameters['duration'])
-        sw_version = '\nsw_version %s' % (session_parameters['version'])
+        experimenter = '\nexperimenter %s' % (lfp_header['experimenter'])
+        comments = '\ncomments %s' % (lfp_header['comments'])
+        duration = '\nduration %d' % (lfp_header['duration'])
+        sw_version = '\nsw_version %s' % (lfp_header['version'])
         num_chans = '\nnum_chans 1'
 
         if egf:
-            sample_rate = '\nsample_rate %d Hz' % (session_parameters['Fs_EGF'])
-            data = struct.pack('<%dh' % (num_samples), *[int(eeg_data) for eeg_data in data.tolist()])
+            sample_rate = '\nsample_rate %d Hz' % (lfp_header['Fs_EGF'])
+            data = struct.pack('<%dh' % (num_samples), *[int(sample) for sample in lfp_ephys_data.tolist()])
             b_p_sample = '\nbytes_per_sample 2'
             num_EEG_samples = '\nnum_EGF_samples %d' % (num_samples)
 
         else:
-            sample_rate = '\nsample_rate %d.0 hz' % (session_parameters['Fs_EEG'])
-            data = struct.pack('>%db' % (num_samples), *[int(eeg_data) for eeg_data in data.tolist()])
+            sample_rate = '\nsample_rate %d.0 hz' % (lfp_header['Fs_EEG'])
+            data = struct.pack('>%db' % (num_samples), *[int(sample) for sample in lfp_ephys_data.tolist()])
             b_p_sample = '\nbytes_per_sample 1'
             num_EEG_samples = '\nnum_EEG_samples %d' % (num_samples)
 
@@ -51,7 +51,7 @@ def write_axona_lfp(filepath, egf_ephys_data, egf_header):
         f.writelines([data, bytes('\r\ndata_end\r\n', 'utf-8')])
 
 
-def intan_to_egf_dicts(intan_data: dict) -> dict:
+def intan_to_lfp_dicts(intan_data: dict) -> dict:
     """
     Collects all the data needed for an egf file
     from the intan data and returns a dictionary
@@ -63,31 +63,34 @@ def intan_to_egf_dicts(intan_data: dict) -> dict:
     signals from auxiliary inputs.
     """
 
-    egf_ephys_data = intan_ephys_to_egf_dict(intan_data)
+    lfp_ephys_data = intan_ephys_to_lfp_dict(intan_data)
 
-    egf_header = intan_to_egf_header_dict(intan_data)
+    lfp_header = intan_to_lfp_header_dict(intan_data)
 
-    return egf_ephys_data, egf_header
+    return lfp_ephys_data, lfp_header
 
 
-def intan_ephys_to_egf_dict(intan_data: dict) -> dict:
+def intan_ephys_to_lfp_dict(intan_data: dict, egf=True) -> dict:
 
     amplifier_sample_rate = float(intan_data['frequency_parameters']['amplifier_sample_rate'])
 
-    egf_sample_rate = 4.8e3
+    if egf:
+        sample_rate = 4.8e3
+    else:
+        sample_rate = 250.0
 
     try:
         assert len(intan_data['amplifier_data']) == len(intan_data['amplifier_channels'])
     except AssertionError:
         raise ValueError('The number of amplifier channels does not match the number of custom channel names. This file may be corrupted.')
 
-    egf_ephys_data = dict()
-    egf_ephys_data['time'] = down_sample_timeseries(intan_data['t_amplifier'].flatten(), amplifier_sample_rate, egf_sample_rate)
+    lfp_ephys_data = dict()
+    lfp_ephys_data['time'] = down_sample_timeseries(intan_data['t_amplifier'].flatten(), amplifier_sample_rate, sample_rate)
     for i in range(len(intan_data['amplifier_data'])):
         if intan_data['amplifier_data'][i].size > 1:
-            egf_ephys_data[intan_data["amplifier_channels"][i]["custom_channel_name"]] = down_sample_timeseries(intan_data['amplifier_data'][i], amplifier_sample_rate, egf_sample_rate)
+            lfp_ephys_data[intan_data["amplifier_channels"][i]["custom_channel_name"]] = down_sample_timeseries(intan_data['amplifier_data'][i], amplifier_sample_rate, sample_rate)
 
-    return egf_ephys_data
+    return lfp_ephys_data
 
 
 
@@ -138,9 +141,18 @@ def down_sample_timeseries(data: np.ndarray, sample_rate: float, new_sample_rate
 
 
 
-def intan_to_egf_header_dict(intan_data: dict) -> dict:
+def intan_to_lfp_header_dict(intan_data: dict, egf=True) -> dict:
     """
     """
-    egf_header = dict()
-
-    return egf_header
+    lfp_header = dict()
+    lfp_header['date'] = 'UNKNOWN'
+    lfp_header['time'] = 'UNKNOWN'
+    lfp_header['experimenter'] = 'UNKNOWN'
+    lfp_header['comments'] = 'UNKNOWN'
+    lfp_header['duration'] = 'UNKNOWN'
+    lfp_header['version'] = 'UNKNOWN'
+    if egf:
+        lfp_header['sample_rate'] = 4.8e3
+    else: #(if eeg)
+        lfp_header['sample_rate'] = 250 #TODO: Check this
+    return lfp_header
